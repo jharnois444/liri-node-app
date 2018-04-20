df04e2ef160acd7fbee1e09866ffa6c8caaa870a
jharnois444 <jharnois444@gmail.com>
2018-04-19 23:27:35 -0400

    Updated API keys

--- liri.js
+++ liri.js
@@ -3,4 +3,11 @@ require("dotenv").config();
 // Dependencies/Linked Files
 const keys = require('./keys.js'),
     spotify = new Spotify(keys.spotify),
-    client = new Twitter(keys.twitter)
+    client = new Twitter(keys.twitter);
+
+const Twitter = require("twitter"),
+    request = require("request"),
+    inquirer = require("inquirer"),
+    Spotify = require("node-spotify-api"),
+    fs = require("fs"),
+    keys = require("./keys.js")
