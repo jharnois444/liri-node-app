9878ccea27b99655ca01ad26fa82004c8d448702
jharnois444 <jharnois444@gmail.com>
2018-04-21 05:11:26 -0400

    updates

--- liri.js
+++ liri.js
@@ -15,7 +15,16 @@ var Twitter = require('twitter'),
 
 // Make it so liri.js can take in one of the following commands:
 
+/**
+ * Capture User Input
+ * @param {string} liriReturn Commands: my-tweets, spotify-this-song, movie-this, do-what-it-says
+ * @param {string} parameter Optional input
+ */
 
+function userInput(liriReturn, parameter) {
+    if (liriReturn) {
+        log('Processing %s command', liriReturn)
+    }
 
 // my-tweets
 switch (liriReturn) {
@@ -55,15 +64,6 @@ function myTweets() {
     })
 }
 
-
-
-// function spotifySong(songName)
-//     spoify.search({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
-
-
-
-// var songName = (process.argv[2] = "spotify-this-song" && process.argv[3]) ? process.argv[3] : "The Sign Ace of Base"
-
 function spotifyThisSong() {
     spotify
     .search({ type: 'track', query: 'All the Small Things' })
@@ -74,3 +74,34 @@ function spotifyThisSong() {
       console.log(err);
     });
 }
+
+function movieThis(movieName) {
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+        // Parse the body of the site and recover just the imdbRating
+        console.log("The movie's title: " + JSON.parse(body).Title +
+                    "\nYear of Release: " + JSON.parse(body).Year +
+                    "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+                    "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
+                    "\nCountry where movie was produced: " + JSON.parse(body).Country +
+                    "\nLanguage: " + JSON.parse(body).Language +
+                    "\nPlot: " + JSON.parse(body).Plot +
+                    "\nFeatured Actors: " + JSON.parse(body).Actors
+                );
+    }
+    });
+}
+
+// function to access "do what it says" from random.txt file
+function doWhatItSays() {
+	
+	// read record from random.txt file
+	fs.readFile('random.txt', "utf8", function(error, data){
+
+		if (error) {
+    		return console.log(error);
+  		}
+	});
+}}
